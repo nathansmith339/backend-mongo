7bdf7463954813346055e59c82c0fb6dd6d592c7
Nathan Smith <smithnathan339@gmail.com>
2018-09-13 23:16:21 -0700

    mongo functionality and test script (#1)
    * added mongo functionality
    * test script to add users

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const Koa = require('koa');
-const app = new Koa();
+const server = require('./lib/server');
 
-const PORT = 8080;
-
-// Send a response to GET requests on route '/'
-app.use(context => {
-    let message = 'Hello World!';
-    context.body = message;
-});
-
-app.listen(PORT);
-
-console.log(`Server running on http://localhost:${PORT}`);
+server.start();
